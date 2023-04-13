fb22f6426e8d05ed2c6d47cb9326fc2dae30ba58
Seigi Gim <80078548+SeigiGim@users.noreply.github.com>
2023-04-13 08:59:41 -0400

    estilos form registro corregidos

--- src/components/index.js
+++ src/components/index.js
@@ -11,27 +11,29 @@ import InfoPerfil from "./infoPerfil/InfoPerfil";
 import Logo from "./logo/Logo";
 import Modal from "./modal/Modal";
 import Navbar from "./navbar/Navbar";
+import NavbarMenu from "./navbar/NavbarMenu";
 import Portfolio from "./portfolio/Portfolio";
 import Publication from "./publication/Publication";
 import Reactions from "./reactions/Reactions";
 import Carousel from "./carousel/Carousel";
 
 export {
-  Banner,
-  Button,
-  Card,
-  Comment,
-  Footer,
-  Form,
-  FormRegister,
-  Image,
-  InfoCard,
-  InfoPerfil,
-  Logo,
-  Modal,
-  Navbar,
-  Portfolio,
-  Publication,
-  Reactions,
-  Carousel,
+	Banner,
+	Button,
+	Card,
+	Comment,
+	Footer,
+	Form,
+	FormRegister,
+	Image,
+	InfoCard,
+	InfoPerfil,
+	Logo,
+	Modal,
+	Navbar,
+	NavbarMenu,
+	Portfolio,
+	Publication,
+	Reactions,
+	Carousel,
 };
