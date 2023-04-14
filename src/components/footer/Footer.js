f21269b092b84370eb1c6a5620bddbbeec32bbb9
Victor Caro <victor.caro.rainao@gmail.com>
2023-04-13 22:15:05 -0400

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "../logo/Logo";
+import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <Logo />
+      <h4>© 2023</h4>
+    </div>
+  );
 };
 
 export default Footer;
